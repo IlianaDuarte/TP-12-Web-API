f90e17e615ef2ae1e1193bb370cb10a4ac3bc924
IlianaDuarte <130664245+IlianaDuarte@users.noreply.github.com>
2024-10-07 09:21:20 -0300

    arreglo de enpoints

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fca651686c5ac083ec3d69a82c6e4f8a9f7e0a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648f36a0ce6b52df5a352d96dd2d80f82af2f404")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
