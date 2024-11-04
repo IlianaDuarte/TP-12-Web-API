981217a69b7c69b9cddff0b28a706edeb02c4915
IlianaDuarte <130664245+IlianaDuarte@users.noreply.github.com>
2024-11-04 10:16:09 -0300

    nada

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686f53e0e12629fa54e7cb9be7b166f9f7b12ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4677c482a6f0e2e14a1eb54ff939b2367462d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
