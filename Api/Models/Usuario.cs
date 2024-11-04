15135118f97441dce1541c2b2c90fa789a27b3c7
IlianaDuarte <130664245+IlianaDuarte@users.noreply.github.com>
2024-11-04 11:25:10 -0300

    Se agrego la carpeta Endpoints

---
+++ Api/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Nombreusuario { get; set; }
+
+    public string? Contrasenia { get; set; }
+
+    public string? Email { get; set; }
+
+    public bool? Habilitado { get; set; }
+
+    public DateTime? Fechacreacion { get; set; }
+
+    public virtual ICollection<Usuariorol> Usuariorols { get; set; } = new List<Usuariorol>();
+}
--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4677c482a6f0e2e14a1eb54ff939b2367462d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981217a69b7c69b9cddff0b28a706edeb02c4915")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
